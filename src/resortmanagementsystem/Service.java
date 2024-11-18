b990c11b2ae2b8799cefd941c8aa4c4a7ac45a45
danahparis21 <micadanah21@gmail.com>
2024-11-18 20:48:29 +0800

    improved services design

--- src/resortmanagementsystem/Service.java
+++ src/resortmanagementsystem/Service.java
@@ -1,76 +1,117 @@
-
 package resortmanagementsystem;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
-import net.proteanit.sql.*;  //rs2xml.jar
+import javax.swing.table.DefaultTableModel;
+import net.proteanit.sql.*;  // rs2xml.jar
 
 /**
  *
  * @author Danah Paris
  */
-public class Service extends JFrame implements ActionListener{
+public class Service extends JFrame implements ActionListener {
     
-    JTable table;
-    JButton back;
+    JTable serviceTable;
+    JButton backButton;
+    JTextField serviceNameField;
+    JButton submitButton;
     
-    Service(){
+    Service() {
         
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
+       
+        ImageIcon icon = new ImageIcon(getClass().getResource("/icons/servicesbg.png"));
+        JLabel headerLabel = new JLabel(icon);
+        headerLabel.setBounds(-20, -10, 1050, 80);
+        add(headerLabel);
+
+        JLabel serviceLabel = new JLabel("Service Name");
+        serviceLabel.setBounds(50, 100, 100, 20);
+        add(serviceLabel);
+
+        serviceNameField = new JTextField();
+        serviceNameField.setBounds(150, 100, 150, 25);
+        serviceNameField.setBackground(Color.white);
+        add(serviceNameField);
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/eight.jpg"));
-        Image i2 =  i1.getImage().getScaledInstance(600, 600, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel image = new JLabel(i3);
-        image.setBounds(500, 0, 600, 600);
-        add(image);
-        
-        
-        JLabel j1 = new JLabel("Service");
-        j1.setBounds(10, 10, 100, 20);
-        add(j1);
-        
-        JLabel j2 = new JLabel("Budget");
-        j2.setBounds(250, 10, 100, 20);
-        add(j2);
-        
-        
-        table = new JTable();
-        table.setBounds(0, 40, 500, 400);
-        add(table);
-        
-        try{
-            Conn c = new Conn();
-            ResultSet rs = c.s.executeQuery("select * from service");
-            table.setModel(DbUtils.resultSetToTableModel(rs));
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        
-        back = new JButton("Back");
-        back.setBackground(Color.BLACK);
-        back.setForeground(Color.white);
-        back.addActionListener(this);
-        back.setBounds(200, 500, 120, 30);
-        add(back);
-        
-        
-        
-        setBounds(300, 200, 1050, 600);
+        serviceTable = new JTable();
+        JScrollPane scrollPane = new JScrollPane(serviceTable);
+        scrollPane.setBounds(0, 150, 1000, 250);
+        add(scrollPane);
+
+        loadTableData("SELECT * FROM serviceMenu");
+
+        submitButton = new JButton("Submit");
+        submitButton.setBackground(Color.BLACK);
+        submitButton.setForeground(Color.white);
+        submitButton.addActionListener(this);
+        submitButton.setBounds(300, 510, 120, 30);
+        add(submitButton);
+
+        backButton = new JButton("Back");
+        backButton.setBackground(Color.BLACK);
+        backButton.setForeground(Color.white);
+        backButton.addActionListener(this);
+        backButton.setBounds(500, 510, 120, 30);
+        add(backButton);
+
+        setBounds(300, 200, 1000, 600);
         setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == submitButton) {
+            try {
+                String serviceName = serviceNameField.getText(); 
+                String query;
+                
+                if (serviceName.isEmpty()) {
+                    query = "SELECT * FROM serviceMenu";  
+                } else {
+                    query = "SELECT * FROM serviceMenu WHERE service_name LIKE '%" + serviceName + "%'";  // Search by service name
+                }
+                loadTableData(query);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(this, "Error loading data: " + e.getMessage());
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == backButton) {
+            setVisible(false);
+            new Reception();
         }
-    
-    public void actionPerformed(ActionEvent ae){
-        setVisible(false);
-        new Reception();
     }
-    
-    public static void main(String[] args){
-            new Service();
+
+    private void loadTableData(String query) {
+    try {
+        Conn conn = new Conn(); 
+        ResultSet rs = conn.s.executeQuery(query);
+        ResultSetMetaData metaData = rs.getMetaData();
+        
+        String[] columnNames = {"Service ID", "Service Name", "Service Price"};
+        
+       
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0); 
+        
+        while (rs.next()) {
+            Object[] row = new Object[metaData.getColumnCount()];
+            for (int i = 0; i < row.length; i++) {
+                row[i] = rs.getObject(i + 1); 
+            }
+            model.addRow(row); 
+        }
+        serviceTable.setModel(model);
+
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(this, "Error executing query: " + e.getMessage());
+        e.printStackTrace();
     }
-    
-    
+      
+    }
+
+    public static void main(String[] args) {
+        new Service();
+    }
 }
